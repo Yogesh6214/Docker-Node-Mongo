07a0e6005277535106c9436be3141a7f4ad8815e
yogesh6214 <globalaudios736@gmail.com>
2025-12-29 15:53:31 +0530

    Setup Env Variable

--- api/src/lib/server.js
+++ api/src/lib/server.js
@@ -3,9 +3,12 @@ import express from "express";
 export const server = () => {
   const httpServer = express();
 
+  const port = process.env.PORT || 3000;
+  console.log(`Port from .env file: ${process.env.PORT}`);
+
   try {
-    httpServer.listen(3000, () => {
-      console.log("Server is listening on port 3000");
+    httpServer.listen(port, () => {
+      console.log(`Server is listening on port: ${port}`);
     });
   } catch (error) {
     throw new Error(`Failed to start server: ${error.message}`);
